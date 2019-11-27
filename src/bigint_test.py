fcdd8cb8aa85109839b7c057c53715b96b84fb41
Curt J. Sampson <cjs@cynic.net>
2019-11-28 00:16:12 +0900

    WIP src/bigint bi_read_dec: read string of decimal digits
    TODO:
    * XXX bi_read_dec is the non-working function here.
      Next step is to set up and call bi_read_udec.
    * XXX should bi_read_dec check for errors, or depend on the caller to
      have determined that it's a valid decmial for this function?
      Probably the latter, and we should provide a function that will
      identify how much of a string starting a given location is valid,
      letting the caller decide if the char after that ends a token.
    
    Primary function (API):
    * bi_read_dec: Handles leading sign, leading zeros, and ensures that
      the bigint is normalized (i.e., no unnecessary leading 00 or FF bytes).
    
    Support functions:
    * bi_read_udec: Reads an unsigned string of decmial digits, with no
      checks for invalid digits, building a probably-not-normalized bigint
      as output. We do this because it turns out to be difficult and
      expensive to determine the normalized ouput size of the binary
      number without actually doing the conversion; consider e.g. "6535"
      vs. "65536" as inputs.
    * bi_x10u: In-place multiply-by-ten for a bigint, with no overflow or
      other error checks.

--- src/bigint_test.py
+++ src/bigint_test.py
@@ -118,3 +118,124 @@ def test_bi_readhex(M, input, bytes):
     bvalue = M.bytes(OUTBUF+1, len(bytes))
     assert (len(bytes),     bytes,  222,) \
         == (M.byte(OUTBUF), bvalue, M.byte(OUTBUF+size-1))
+
+####################################################################
+#   Tests: bi_read_dec
+
+#   Memory locations of buffers used in tests.
+#   These deliberately cross page boundaries for non-trivial buffer sizes.
+TIN_ADDR  = 0x6FFE
+TOUT_ADDR = 0x71FE
+TSCR_ADDR = 0x73FE
+
+@pytest.mark.parametrize('value', [
+    [0x03],
+    [0x19],                         # maximum 1byte input without overflow
+    [0x19, 0x99],                   # maximum 2byte input without overflow
+    [0x02, 0xab, 0xcd, 0xef, 0x10], # 11 billion or so
+    [0x01] + [0xff]*254,            # maximum length
+])
+def test_bi_x10u(M, value):
+    S = M.symtab
+
+    decval   = int.from_bytes(value, byteorder='big', signed=False)
+    decval10 = decval * 10
+    expected = decval10.to_bytes(len(value), byteorder='big', signed=False)
+    expected = list(expected)
+    print('bi_x10u: {}={} -> {}={}'.format(value, decval, decval10, expected))
+    buflen = len(value)
+
+    #   Guard bytes around scratch buffer show we're not writing outside it.
+    M.depword(TSCR_ADDR-1, 211)
+    M.depword(TSCR_ADDR+buflen, 212)
+    M.depword(S.bufSptr, TSCR_ADDR-1)
+
+    M.deposit(TOUT_ADDR-1, [221] + value + [222])   # guard bytes
+    M.depword(S.buf0ptr, TOUT_ADDR-1)
+
+    M.deposit(S.buf0len, buflen)
+    M.call(S.bi_x10u)
+    assert [211] == M.bytes(TSCR_ADDR-1, 1)
+    assert [212] == M.bytes(TSCR_ADDR+buflen, 1)
+    assert [221] + expected + [222] == M.bytes(TOUT_ADDR-1, len(value)+2)
+
+    #   Turn this on to get a sense of how fast/slow this is.
+    #assert not M.mpu.processorCycles
+
+@pytest.mark.parametrize('input, bytes', [
+    (b'0',                  [0x00, 0x00]),
+    (b'0000',               [0x00, 0x00]),
+    (b'9',                  [0x00, 0x09]),
+    (b'10',                 [0x00, 0x0A]),
+    (b'65432',              [0xFF, 0x98]),
+    (b'65432',              [0x00, 0xFF, 0x98]),
+    (b'65432',              [0x00, 0x00, 0xFF, 0x98]),
+    (b'78912345678901',     [0x47, 0xc5, 0x36, 0x55, 0x24, 0x35]),
+])
+def test_bi_read_udec(M, input, bytes):
+    print('bi_read_udec:', input, bytes)
+    S = M.symtab
+
+    osize = len(bytes)
+    M.deposit(S.buf0len, osize)
+    M.deposit(TOUT_ADDR-1, [240] + [0]*osize + [241])
+    M.depword(S.buf0ptr, TOUT_ADDR-1)
+
+    M.deposit(TIN_ADDR-1, b'\xDD' + input + b'\xDF')    # guard bytes
+    M.depword(S.buf1ptr, TIN_ADDR)
+
+    M.deposit(TSCR_ADDR-1, 250)                         # guard bytes
+    M.deposit(TSCR_ADDR+osize, 251)
+    M.depword(S.bufSptr, TSCR_ADDR-1)
+
+    M.call(S.bi_read_udec, R(y=len(input)), trace=0)
+    assert [240] + bytes + [241] == M.bytes(TOUT_ADDR-1, osize+2)
+    assert [250] == M.bytes(TSCR_ADDR-1, 1)
+    assert [251] == M.bytes(TSCR_ADDR+osize, 1)
+
+@pytest.mark.skip(msg='This test can take up to 10 seconds to run')
+def test_bi_read_udec_max(M):
+    S = M.symtab
+
+    input = b'9'*255
+    expected = list(int(input).to_bytes(128, byteorder='big', signed=False))
+    print(int(input))
+    print(expected)
+
+    M.deposit(TIN_ADDR-1, [122] + list(input) + [133])  # guard bytes
+    M.depword(S.buf1ptr, TIN_ADDR)
+
+    osize = len(expected)
+    M.deposit(S.buf0len, osize)
+    M.deposit(TOUT_ADDR-1, [155] + [0]*osize + [166])
+    M.depword(S.buf0ptr, TOUT_ADDR-1)
+    M.depword(S.bufSptr, TSCR_ADDR-1)
+
+    M.call(S.bi_read_udec, R(y=len(input)), trace=0)
+    assert [155] + expected + [166] == list(M.bytes(TOUT_ADDR-1, osize+2))
+
+    #   Show this takes 3,541,444 cyles, about 3.5 seconds on 1 MHz 6502.
+    #assert not M.mpu.processorCycles
+
+@pytest.mark.parametrize('input, bytes', [
+    (b"0",              [0x00]),
+    (b"+0",             [0x00]),
+    (b"-0",             [0x00]),
+    (b"0000",           [0x00]),
+    (b"+0001",          [0x01]),
+    (b"-00001",         [0xFF]),
+])
+def test_bi_read_dec(M, input, bytes):
+    print('bi_read_dec:', input, bytes)
+    S = M.symtab
+
+    M.deposit(TIN_ADDR, input)
+    M.depword(S.buf3ptr, TIN_ADDR)
+    M.depword(S.buf2ptr, TOUT_ADDR)
+    size = len(bytes) + 2               # length byte + value + guard byte
+    M.deposit(TOUT_ADDR, [222] * size)  # 222 ensures any 0s really were written
+
+    M.call(S.bi_read_dec, R(y=len(input)))
+    bvalue = M.bytes(TOUT_ADDR+1, len(bytes))
+    assert (len(bytes),     bytes,  222) \
+        == (M.byte(TOUT_ADDR), bvalue, M.byte(TOUT_ADDR+size-1))
