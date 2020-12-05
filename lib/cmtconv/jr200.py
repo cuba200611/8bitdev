d150fca4888f7d8ce82c30104c4fb59bdb0ce40c
Stuart Croy <stuartcroy@mac.com>
2020-12-05 21:47:23 +0000

    Clean up and refactoring to support explicitly creating objects from header info and data instead of just raw bytes from cjr files.

--- lib/cmtconv/jr200.py
+++ lib/cmtconv/jr200.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import sys
 from collections import namedtuple
 from enum import Enum
+from testmc.memimage import MemImage
 
 # General approach is to use layered abstractions in
 # a simple top-down parser.
@@ -27,28 +28,21 @@ from enum import Enum
 # make cutoff a parameter & command line option (?)
 # gracefully deal with end of samples and try to read multiple files
 #
-# Generate file header and blocks from cjr file format
 # Generate cjr file from header and blocks
-# Generate audio from blocks
-# Split out most code into a library
-# Add pytest tests
 # Documentation
 #
 
-class NotImplementedError(Exception):
-    ''' Not Implemented ''' # FIXME: remove when done
-
 def err(*args, **kwargs):
-    print(*args, file = sys.stderr, **kwargs)
+    print(*args, file=sys.stderr, **kwargs)
 
 def log(*args, **kwargs):
-    print(*args, file = sys.stderr, **kwargs)
+    print(*args, file=sys.stderr, **kwargs)
 
 def info(*args, **kwargs):
-    print(*args, file = sys.stderr, **kwargs)
+    print(*args, file=sys.stderr, **kwargs)
 
 def debug(*args, **kwargs):
-    print(*args, file = sys.stderr, **kwargs)
+    print(*args, file=sys.stderr, **kwargs)
 
 # samples   : [ float ]
 # ->
@@ -59,7 +53,7 @@ def samples_to_levels(samples):
         sample_min = min(samples)
         cutoff = sample_min + 0.45 * (sample_max - sample_min)
         debug('Max: %d, min: %d, cutoff: %d' %
-                (sample_max, sample_min, cutoff))
+              (sample_max, sample_min, cutoff))
         return tuple(x > cutoff for x in samples)
     else:
         return ()
@@ -81,7 +75,7 @@ def levels_to_timed_edges(levels, sample_dur):
         for (i, l) in enumerate(levels):
             t = sample_dur * i
             if l != last_level:
-                edges.append( (t, l, t - last_t) )
+                edges.append((t, l, t - last_t))
                 last_t = t
                 last_level = l
     return tuple(edges)
@@ -107,7 +101,7 @@ def next_space(edges, i_next, edges_needed):
             consecutive = 0
         i += 1
     raise Exception('unable to find %d consecutive edges of space'
-                        % edges_needed)
+                    % edges_needed)
 
 # edge      : ( ( float, bool, float ), )
 # ->
@@ -162,16 +156,16 @@ class Decoder(object):
                 e = edges[idx + i]
                 if not is_mark(e):
                     dur = 1000000 * e[2]
-                    raise Exception('Expected %d mark edges: %f (at %f) '
-                        % (self.mark_edges, dur, e[0]))
+                    raise Exception('Expected %d mark edges: %f (at %f) ' %
+                                    (self.mark_edges, dur, e[0]))
             return (idx + self.mark_edges, 1)
         else:
             for i in range(self.space_edges):
                 e = edges[idx + i]
                 if is_mark(e):
                     dur = 1000000 * e[2]
-                    raise Exception('Expected %d space edges: %f (at %f) '
-                        % (self.space_edges, dur, e[0]))
+                    raise Exception('Expected %d space edges: %f (at %f) ' %
+                                    (self.space_edges, dur, e[0]))
             return (idx + self.space_edges, 0)
 
     # edges     : ( ( float, bool, float ), )
@@ -199,8 +193,8 @@ class Decoder(object):
         if bits == (1,):
             return i_next
         else:
-            raise Exception('Expected start bits: ( 1, ), got: %s '
-                % str(bits))
+            raise Exception('Expected start bits: ( 1, ), got: %s ' %
+                            str(bits))
 
     # edges     : ( ( float, bool, float ), )
     # i_next    : int
@@ -212,8 +206,8 @@ class Decoder(object):
         if bits == (0, 0, 0):
             return i_next
         else:
-            raise Exception('Expected stop bits ( 0, 0, 0 ), got: %s '
-                            % str(bits))
+            raise Exception('Expected stop bits ( 0, 0, 0 ), got: %s ' %
+                            str(bits))
 
     # We use the definition of mark/1 and space/0 as per the service manual,
     # but the actual bits seem to be inverted. They are also little-endian,
@@ -245,31 +239,100 @@ class Decoder(object):
             res.append(x)
         return (i_next, tuple(res))
 
+class FileType(Enum):
+    '''File type as it appears in the file header'''
+    BASIC   = 0
+    BINARY  = 1
+
+class BaudRate(Enum):
+    '''Baud rate as it appears in the file header'''
+    BAUD_2400   = 0
+    BAUD_600    = 1
 
 class FileHeader(object):
-    # Format is as below:
-    # 0-1: signature/header - should be 2, 42
-    # 2: block number
-    # 3: length: 26
-    # 4-5: pad: 255
-    # 7-21: file name
-    # 22: BASIC(0)/Binary(1)
-    # 23: Baud rate 2400 - 0, 600 - 1
-    # 25-32: pad: 255
-    # 33: checksum
+    '''This represents the 33 byte file header.
 
-    def __init__(self, raw_bytes):
+    Format is as below:
+    0-1: signature/header - should be 2, 42
+    2: block number
+    3: length: 26
+    4-5: pad: 255
+    6-21: file name
+    22: BASIC(0)/Binary(1)
+    23: Baud rate 2400 - 0, 600 - 1
+    24-31: pad: 255
+    32: checksum
+    '''
+
+    def __init__(self, header, block_number, length, pad0, filename, filetype,
+                 baud_rate, pad1, checksum, raw_bytes):
+        '''Caller is responsible for ensuring interpreted values match bytes'''
+        self.header         = header
+        self.block_number   = block_number
+        self.length         = length
+        self.pad0           = pad0
+        self.filename       = filename
+        self.filetype       = filetype
+        self.baud_rate      = baud_rate
+        self.pad1           = pad1
+        self.checksum       = checksum
+        self.raw_bytes      = raw_bytes
+
+    @staticmethod
+    def from_bytes(raw_bytes):
+        '''Construct a block header from bytes'''
         b = raw_bytes
-        self.raw_bytes      = b
-        self.header         = (b[0], b[1])
-        self.block_number   = b[2]
-        self.length         = b[3]
-        self.pad0           = (b[4], b[5])
-        self.filename       = ''.join(chr(x) for x in b[6:22] if x > 0)
-        self.filetype       = b[22]
-        self.baud_rate      = b[23]
-        self.pad1           = tuple(b[24:31])
-        self.checksum       = b[32]
+        return FileHeader(
+            (b[0], b[1]),
+            b[2],
+            b[3],
+            (b[4], b[5]),
+            ''.join(chr(x) for x in b[6:22] if x > 0),
+            FileType(b[22]),
+            BaudRate(b[23]),
+            tuple(b[24:32]),
+            b[32],
+            b)
+
+    @staticmethod
+    def make(file_name, file_type, baud_rate):
+        '''Construct a block header for given args'''
+        if len(file_name) > 16:
+            raise ValueError('file name too long (16 characters max): "{}"' %
+                             file_name)
+        header          = (2, 42)
+        block_number    = 0
+        length          = 26
+        pad0            = (0xff,) * 2
+        pad1            = (0xff,) * 8
+        filetype        = 0 if file_type == FileType.BASIC else 1
+        baudrate        = 0 if baud_rate == BaudRate.BAUD_2400 else 1
+
+        raw_bytes = bytearray()
+        raw_bytes.extend(header)
+        raw_bytes.append(block_number)
+        raw_bytes.append(26)
+        raw_bytes.extend(pad0)
+        raw_bytes.extend(ord(x) for x in file_name)
+        raw_bytes.extend((0x00,) * (16-len(file_name)))
+        raw_bytes.append(filetype)
+        raw_bytes.append(baudrate)
+        raw_bytes.extend(pad1)
+        chksum = sum(raw_bytes) % 256
+        raw_bytes.append(chksum)
+
+        return FileHeader(
+            header,
+            block_number,
+            length,
+            pad0,
+            file_name,
+            file_type,
+            baud_rate,
+            pad1,
+            chksum,
+            raw_bytes
+        )
 
     def __str__(self):
         s = ''
@@ -279,28 +342,66 @@ class FileHeader(object):
         s += 'Length: {}\n'.format(self.length)
         s += 'Pad0: {}\n'.format(tuple(hex(x) for x in  self.pad0))
         s += 'Filename: "{}"\n'.format(self.filename)
-        s += 'File type: {}\n'.format(hex(self.filetype))
-        s += 'Baud rate: {}\n'.format(hex(self.baud_rate))
+        s += 'File type: {}\n'.format(self.filetype)
+        s += 'Baud rate: {}\n'.format(self.baud_rate)
         s += 'Pad1: {}\n'.format(tuple(hex(x) for x in self.pad1))
         s += 'Checksum: {} (calculated: {})\n'.format(
-                hex(self.checksum), hex(sum(self.raw_bytes[:-1]) % 256))
+            hex(self.checksum), hex(sum(self.raw_bytes[:-1]) % 256))
         return s
 
     # FIXME: add proper checksum calc to allow checking
 
 
-class BlockHeader( object ):
+class BlockHeader(object):
+    '''Interprets the block header'''
 
-    def __init__(self, raw_bytes):
+    def __init__(self, header, block_number, length, addr, raw_bytes):
+        '''Caller is responsible for ensuring bytes match interpretation'''
+        self.header         = header
+        self.block_number   = block_number
+        self.length         = length
+        self.addr           = addr
+        self.raw_bytes      = raw_bytes
+
+    @staticmethod
+    def from_bytes(raw_bytes):
+        '''Construct the block header from raw bytes'''
         b = raw_bytes
-        self.raw_bytes      = b
-        self.header         = (b[0], b[1])
-        self.block_number   = b[2]
-        self.length         = b[3]
-        self.addr           = b[4] * 256 + b[5]
-        if self.header != (2, 42):
-            raise Exception('Invalid signature in header: {}'.format(
-                                self.header))
+        header = (b[0], b[1])
+        if header != (2, 42):
+            raise ValueError('Invalid signature in header: {}'.format(header))
+
+        length = b[3]
+        if length == 0:
+            length = 256
+        addr = b[4] * 256 + b[5]
+
+        return BlockHeader(header, b[2], length, addr, raw_bytes)
+
+    @staticmethod
+    def make(block_number, length, addr):
+        '''Build a block header from details'''
+        if block_number < 0 or block_number > 255:
+            raise ValueError('Block number must be in range 0-255: {}' %
+                block_number)
+        if length < 0 or length > 256:
+            raise ValueError('Length must be in range 0-256: {}' % length)
+        if addr < 0 or addr > 0xffff:
+            raise ValueError('Address must be in range 0x0000-0xffff: {}' %
+                addr)
+        header = (2, 42)
+        b = bytearray()
+        b.extend(header)
+        b.append(block_number)
+        b.append(0 if length == 256 else length)
+        b.append(addr // 256)
+        b.append(addr % 256)
+        return BlockHeader(header, block_number, length, addr, b)
+
+    @staticmethod
+    def make_tail():
+        '''Make a tail block'''
+        return BlockHeader.make(255, 255, 0)
 
     def __str__(self):
         s = ''
@@ -312,6 +413,8 @@ class BlockHeader( object ):
         return s
 
     def is_tail(self):
+        #FIXME: what happens when we try to save the whole 64k?
+        #Is the block number alone enough?
         return self.block_number == 255 and self.length == 255
 
     def calc_checksum(self):
@@ -321,17 +424,19 @@ class BlockHeader( object ):
             return sum(self.raw_bytes) % 256
 
 class Block(object):
+    '''Represents a block of data'''
 
-    def __init__(self, header, bytes, checksum):
+    def __init__(self, header, data, checksum):
         self.header     = header
-        self.data       = bytes
+        self.data       = data
         self.checksum   = checksum
 
     def __str__(self):
         s = ''
         s += str(self.header)
         for (i, x) in enumerate(self.data):
-            if i % 20 == 0: s+= '\n'
+            if i % 20 == 0:
+                s+= '\n'
             s += '  {0:02x}'.format(x)
         s += '\n\nChecksum: %x' % self.checksum
         return s
@@ -343,14 +448,14 @@ class Block(object):
             header_chksum = self.header.calc_checksum()
             data_chksum = sum(self.data) % 256
             chksum = (header_chksum + data_chksum) % 256
-            debug('header_chksum: %d, data_chksum: %d, chksum: %d'
-                    % (header_chksum,data_chksum,chksum))
+            debug('header_chksum: %d, data_chksum: %d, chksum: %d' %
+                  (header_chksum,data_chksum, chksum))
             return chksum
 
 File = namedtuple('File', 'header blocks')
 
 class FileReader(object):
-    def __init__( self ):
+    def __init__(self):
         self.baud600_decoder = Decoder(8, 4)
         self.baud2400_decoder = Decoder(2, 1)
 
@@ -358,13 +463,13 @@ class FileReader(object):
     # first byte
     def read_leader(self, edges, i_next):
         i_next = next_space(edges, i_next, 100)
-        debug('Leader cycles detected at %d - %fs (%s)'
-            % (i_next, edges[i_next][0], str(edges[i_next][1])))
+        debug('Leader cycles detected at %d - %fs (%s)' %
+              (i_next, edges[i_next][0], str(edges[i_next][1])))
 
         # Read up to the start bit of the first byte
         (i_next, _) = eat_until_mark(edges, i_next)
-        debug('Start of data at %d - %fs (%s)'
-            % (i_next, edges[i_next][0], edges[i_next][1]))
+        debug('Start of data at %d - %fs (%s)' %
+              (i_next, edges[i_next][0], edges[i_next][1]))
 
         return i_next
 
@@ -372,9 +477,9 @@ class FileReader(object):
         i_next = self.read_leader(edges, i_next)
 
         (i_next, header_bytes) = self.baud600_decoder.eat_bytes(
-                                    edges, i_next, 33)
+            edges, i_next, 33)
         #debug(header_bytes)
-        hdr = FileHeader(header_bytes)
+        hdr = FileHeader.from_bytes(header_bytes)
         debug(hdr)
 
         debug('i_next: %d( %f )' % (i_next, edges[i_next][0]))
@@ -386,7 +491,7 @@ class FileReader(object):
 
         # Read the block header
         (i_next, block_header_bytes) = bit_decoder.eat_bytes(edges, i_next, 6)
-        block_hdr = BlockHeader(block_header_bytes)
+        block_hdr = BlockHeader.from_bytes(block_header_bytes)
         #debug(block_hdr)
         if block_hdr.is_tail():
             return (i_next, Block(block_hdr, [], 0))
@@ -394,7 +499,7 @@ class FileReader(object):
             # Read the data
             actual_length = block_hdr.length if block_hdr.length > 0 else 256
             (i_next, block_data_bytes) = bit_decoder.eat_bytes(
-                                            edges, i_next, actual_length + 1)
+                edges, i_next, actual_length + 1)
             block_data_chksum = block_data_bytes[-1]
             block_data_bytes = block_data_bytes[:-1]
 
@@ -454,7 +559,7 @@ def checksums_valid(blocks):
 def blocks_to_bytes(blocks):
     res = bytearray()
     for block in blocks:
-        res.extend( block.data )
+        res.extend(block.data)
     return res
 
 
@@ -463,13 +568,31 @@ def blocks_to_bytes(blocks):
 # filename  : str
 # data      : bytearray
 # addr      : int
-# baud      : int
 # filetype  : int
+# baud      : int
 # ->
 # File
-def bytes_to_file(filename, data, addr, baud, filetype):
-    raise NotImplementedError
-
+def bytes_to_file(filename, data, addr, filetype, baud):
+    '''Convert file info and bytes to File object'''
+    file_header = FileHeader.make(filename, filetype, baud)
+    blocks = []
+    idx = 0
+    a = addr
+    bn = 1
+    remaining = len(data)
+    while remaining > 0:
+        block_size = min(256, remaining)
+        block_data = data[idx:idx + block_size]
+        bh = BlockHeader.make(bn, block_size, a)
+        chksum = (sum(block_data) + bh.calc_checksum()) % 256
+        blocks.append(Block(bh, block_data, chksum))
+        idx += block_size
+        a += block_size
+        bn += 1
+        remaining -= block_size
+    #blocks.append(Block(BlockHeader.make_tail(), tuple(), 0))
+    blocks.append(Block(BlockHeader.make(255, 255, a), (), 0))
+    return File(file_header, tuple(blocks))
 
 # Convert a 'cjr' file to a file header and blocks
 #
@@ -477,14 +600,14 @@ def bytes_to_file(filename, data, addr, baud, filetype):
 # ->
 # File
 def cjr_to_file(bytes):
-    file_hdr = FileHeader(bytes[0:33])
+    file_hdr = FileHeader.from_bytes(bytes[0:33])
     debug(file_hdr)
     blocks = []
     i = 33
     while True:
         data = bytes[i:i+6]
         debug('Header raw data: {}'.format(tuple(data)))
-        block_hdr = BlockHeader(bytes[i:i+6])
+        block_hdr = BlockHeader.from_bytes(bytes[i:i+6])
         debug(block_hdr)
         if block_hdr.is_tail():
             blk = Block(block_hdr, (), 0)
@@ -528,7 +651,7 @@ class Encoder(object):
     def __init__(self, mark_edges, space_edges):
         self.mark_edges     = mark_edges
         self.space_edges    = space_edges
-    
+
     # bit
     def encode_bit(self, bit):
         # FIXME: push into init
@@ -608,7 +731,7 @@ class FileEncoder(object):
         debug(len(data))
         debug(' '.join(hex(x) for x in data))
         data_edges = encoder.encode_bytes(data)
-        return (sound(leader_edges + data_edges ),)
+        return (sound(leader_edges + data_edges),)
 
     def blocks(self, encoder, blocks):
         edges = ()
@@ -617,13 +740,13 @@ class FileEncoder(object):
         return edges
 
     def encode_file(self, f):
-        if f.header.baud_rate == 0:
+        if f.header.baud_rate == BaudRate.BAUD_2400:
             encoder = self.baud2400_encoder
-        elif f.header.baud_rate == 1:
+        elif f.header.baud_rate == BaudRate.BAUD_600:
             encoder = self.baud600_encoder
         else:
-            raise Exception('Unsupported baud setting: %d'
-                                % f.header.baud_rate)
+            raise Exception('Unsupported baud setting: %d' %
+                            f.header.baud_rate)
         return self.header(f.header) + self.blocks(encoder, f.blocks)
 
 
@@ -651,7 +774,7 @@ def edges_to_samples(edges, sample_dur, silence, low, high):
                 res.extend(sample_lvl for _ in range(int(dur/sample_dur)))
                 lvl = not lvl
         else:
-            raise Exception( 'Unknown audio marker' )
+            raise Exception('Unknown audio marker')
     return res
 
 
@@ -663,3 +786,9 @@ def edges_to_samples(edges, sample_dur, silence, low, high):
 # ( double, )
 def file_to_samples(f, rate):
     raise NotImplementedError
+
+# Convert MemImage to blocks
+#
+# mi    : MemImage
+# fname : filename
+#
